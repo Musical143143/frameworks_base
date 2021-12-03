60499d668143681d5e2724cda8bdf3f9251f5d23
trpsl <trpsl755@gmail.com>
2021-12-03 23:12:32 +0100

    SystemUI: [udfps] Allow devices to pulse onFingerDown instead of waking the screen
    Change-Id: If31e9b4c9f1454c22303d8ca0f7d6cbec3786ebf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -49,6 +49,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.util.Log;
@@ -398,8 +399,13 @@ private static void xaiomiTouchFeature(int arg) {
                     return;
                 }
                 if (vendorCode == mUdfpsVendorCode) {
-                    mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
-                            PowerManager.WAKE_REASON_GESTURE, TAG);
+                    if (mContext.getResources().getBoolean(R.bool.config_pulseOnFingerDown)) {
+                        mContext.sendBroadcastAsUser(new Intent(PULSE_ACTION),
+                                new UserHandle(UserHandle.USER_CURRENT));
+                    } else {
+                        mPowerManager.wakeUp(mSystemClock.uptimeMillis(),
+                                PowerManager.WAKE_REASON_GESTURE, TAG);
+                    }
                     onAodInterrupt(0, 0, 0, 0);
                 }
             }
